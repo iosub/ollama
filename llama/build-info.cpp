f616cc1b20ef5e0076f6bbb2add8d4cbe0359327
iosub <iosub@gabiges.com>
2025-11-04 14:14:51 -0500

    b6845

--- llama/build-info.cpp
+++ llama/build-info.cpp
@@ -1,4 +1,4 @@
 int LLAMA_BUILD_NUMBER = 0;
-char const *LLAMA_COMMIT = "3cfa9c3f125763305b4226bc032f1954f08990dc";
+char const *LLAMA_COMMIT = "73a48c9790d320476b3e5ef75bda09f2f8269e6e";
 char const *LLAMA_COMPILER = "";
 char const *LLAMA_BUILD_TARGET = "";
